685fdc6256112830d20d73aa554359e275b4f6a9
Syrae <surajitmandal@Syrae.local>
2026-01-01 19:34:31 +0530

    Initial commit

---
+++ expence_tracker.py
@@ -0,0 +1,57 @@
+def menu():
+    
+    print("\n\t\t1.Add Expenses")
+    print("\t\t2.View Expenses ")
+    print("\t\t3.View Total")
+    print("\t\t4.Exit")
+
+def get_options(options):
+    match options:
+        case 1:
+            return "\n\n\tAdding expense"
+        case 2:
+            return "\n\n\tViewing expense"
+        case 3:
+            return " \n\n\tViewing Total"
+        case 4:
+            return "\n\n\tExiting Expense Tracker"
+        case _:
+            return "\n\n\tEnter a valid choice!"
+
+def main():
+    print("\n\n\t\tWelcome to Expense Tracker.")
+    print("\n\t\tYour one stop budgeting solution.")
+    expenses = [] 
+    while True :
+        try: 
+         menu()
+         choice= int(input("\n\tChoose an option : "))
+         print(get_options(choice))
+         if choice ==1:
+            amount = float(input("Amount : "))
+            category = input("Category : ")
+            descriptions = input("Descriptions : ")
+            expense = {
+                "Amount" : amount,
+                "Category" : category,
+                "Descriptions" : descriptions
+            }
+            expenses.append(expense)
+         elif choice == 2:
+             for expense in expenses:
+                 print(f"\n\tAmount : {expense['Amount']}")
+                 print(f"\tCategory :{expense['Category']}")    
+                 print(f"\tDescriptions :{expense['Descriptions']}")   
+         elif choice == 3 :
+                total = 0
+                for expense in expenses:
+                    total+=expense["Amount"]
+                print(f"\t\t = {total}")
+            
+         elif choice ==4:
+             break
+        except ValueError:
+            print("Enter a number . No alphabets allowed.")
+
+if __name__=="__main__":
+    main()
